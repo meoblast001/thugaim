683ea672d5ce06b5d931b180f8700718cd309e72
Braden Walters <vc@braden-walters.info>
2013-12-10 23:20:23 -0500

    Starts to instructions view; Game view keeps screen on

---
+++ src/info/meoblast001/thugaim/Instructions.java
@@ -0,0 +1,42 @@
+/*
+Copyright (C) 2013 Braden Walters
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package info.meoblast001.thugaim;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Instructions extends Activity
+{
+  /**
+  Called when the activity is first created.
+  */
+  @Override
+  public void onCreate(Bundle saved_instance_state)
+  {
+    super.onCreate(saved_instance_state);
+    setContentView(R.layout.instructions);
+  }
+
+  public void goToGame(View view)
+  {
+    startActivity(new Intent(view.getContext(), Thugaim.class));
+    finish();
+  }
+}
